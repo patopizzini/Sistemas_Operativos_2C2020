ba9cd5226514156a01225f7cf77b3ed8989eb421
Juan Manuel Gonzalez <juanmg0511@gmail.com>
2020-11-21 19:52:41 -0300

    esqueleto del proceso ppal, para poder lanzarlo y pararlo

--- TP1/Grupo3/original/pprincipal.sh
+++ TP1/Grupo3/original/pprincipal.sh
@@ -9,5 +9,19 @@
 #
 #TP1 - pprincipal
 
+#Configuración del intevalo entre ejecuciones, 8 segundos por enunciado
+INTERVALO_SEGUNDOS=8
+NUMERO_CICLO=0
 
+#Ciclo infinito a repetir según el intervalo configurado
+while true
+do
+	#El primer ciclo es 1
+	let "NUMERO_CICLO++"
+
+	#Sleep para tener una pausa entre ejecuciones (parametrizable)
+	sleep "$INTERVALO_SEGUNDOS"
+done
+
+#Retorno al SO
 exit 0
