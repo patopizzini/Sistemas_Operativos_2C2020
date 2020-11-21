e4a6920982aedb7717bf23d638502574e0442f7b
Andrés SIlvestri <silvahlaravel@gmail.com>
2020-11-21 01:18:39 -0300

    iniciarambiente-esqueleto
    se agregan algunos métodos básicos para ir ampliando luego.

--- TP1/Grupo3/original/iniciarambiente.sh
+++ TP1/Grupo3/original/iniciarambiente.sh
@@ -9,7 +9,6 @@
 #
 #TP1 - iniciarambiente
 
-
 #Variables con los datos de la instalación
 TIPO_INSTALACION="INSTALACION"
 ESTADO_INSTALACION="NO LISTA"
@@ -37,6 +36,7 @@ PATH_LOG_PROCESO_PPAL="$PATH_BASE/so7508/pprincipal.log"
 #Variable para los strings que van a stdout y al log
 MENSAJE=""
 CODIGOS="GRUPO;DIRINST;DIRBIN;DIRMAE;DIRIN;DIRRECH;DIRPROC;DIROUT;INSTALACION"
+ARCHIVOS="archivo1.txt;archivo2.txt;archivo3.txt"
 IDENTIFICADOR_ERRONEO=""
 PATH_ERRONEO=""
 
@@ -56,19 +56,21 @@ validar_identificador(){
 }
 
 validar_directorio(){
-    if [ ! -f "$2" ]; 
+    if [ ! -d "$2" ]; 
     then
-        MENSAJE="El archivo \""$2\"" no existe, correspondiente al código: \""$1\""."
+        MENSAJE="El directorio \""$2\"" no existe, correspondiente al código: \""$1\""."
         echo "$MENSAJE"
         log_message "$MENSAJE"
         PATH_ERRONEO="$1"
     else	
-        MENSAJE="El archivo \""$2\"" fue encontrado, correspondiente al código: \""$1\""."
+        MENSAJE="El directorio \""$2\"" fue encontrado, correspondiente al código: \""$1\""."
         echo "$MENSAJE"
         log_message "$MENSAJE"
     fi
 }
 
+#Leer el archivo instalarTP.conf y verificar que todos esos directorios existen
+# | Verificar directorios |
 validar_configuracion() {
     let CANTIDAD=1
     while read -r LINEA
@@ -86,15 +88,20 @@ validar_configuracion() {
             validar_identificador "$IDENTIFICADOR" "$VALOR" "$CANTIDAD"
             if [ "$IDENTIFICADOR_ERRONEO" != "" ]
             then
-                MENSAJE="En la posición: \""$CANTIDAD\"" del archivo instalarTP.conf está mal especificado el código: \""$IDENTIFICADOR_ERRONEO\"" para resolverlo ejecute la reparación." 
+                MENSAJE="En la posición: \""$CANTIDAD\"" del archivo instalarTP.conf está mal especificado el código: \""$IDENTIFICADOR_ERRONEO\""." 
+                echo "$MENSAJE"
+                log_message "$MENSAJE"
+                MENSAJE="Por favor vuelva a ejecutar instalarTP con la opción reparar." 
                 echo "$MENSAJE"
                 log_message "$MENSAJE"
                 break
             fi
-
             if [ "$PATH_ERRONEO" != "" ]
             then
-                MENSAJE="En la posición: \""$CANTIDAD\"" del archivo instalarTP.conf está mal especificado el directorio para el código: \""$PATH_ERRONEO\"" para resolverlo ejecute la reparación." 
+                MENSAJE="En la posición: \""$CANTIDAD\"" del archivo instalarTP.conf está mal especificado el directorio para el código: \""$PATH_ERRONEO\""."
+                echo "$MENSAJE"
+                log_message "$MENSAJE"
+                MENSAJE="Por favor vuelva a ejecutar instalarTP con la opción reparar." 
                 echo "$MENSAJE"
                 log_message "$MENSAJE"
                 break
@@ -108,6 +115,67 @@ validar_configuracion() {
     done < "$PATH_CONFIGURACION"
 }
 
+#Los archivos del directorio de tablas maestras deben tener permiso de lectura
+#Los archivos del directorio de ejecutables deben tener permiso de ejecución
+# | Verificar permisos |
+validar_permisos() {
+    DIRECTORIO_TEMPORAL="/home/andres/Documents/SISTEMAS OPERATIVOS/TEST/"
+    chmod -R u=r,g=r,o=r "$DIRECTORIO_TEMPORAL"
+    #chmod -R u=rwx,g=rwx,o=rwx "$PATH_EJECUTABLES"
+    echo "listo"
+}
+
+#Ir al directorio de tablas maestras y verificar que existan
+# | Verificar archivos |
+validar_archivos() {
+    DIRECTORIO_TEMPORAL="/home/andres/Documents/SISTEMAS OPERATIVOS/TEST/"
+    ARCHIVOS=${ARCHIVOS//;/$'\n'}  # change the semicolons to white space
+    for word in $ARCHIVOS
+    do
+        CONCATENADO="${DIRECTORIO_TEMPORAL}${word}"
+        if [ ! -f "$CONCATENADO" ]; 
+        then
+            MENSAJE="Falta el archivo del directorio de tablas maestras:  \""$CONCATENADO\""."
+            echo "$MENSAJE"
+            log_message "$MENSAJE"
+            MENSAJE="Por favor vuelva a ejecutar instalarTP con la opción reparar."
+            echo "$MENSAJE"
+            log_message "$MENSAJE"
+        else	
+            MENSAJE="Se encontró el archivo del directorio de tablas maestras:  \""$CONCATENADO\""."
+            echo "$MENSAJE"
+            log_message "$MENSAJE"
+        fi
+    done
+}
+
+
+#Todos los identificadores del archivo de configuración deben convertirse en variables de ambiente
+# | Variables de ambiente |
+variables_ambiente() {
+    echo "validar_ambiente"
+}
+
+#Invocar al script pprincipal ADVERTENCIA: no invocar el proceso si ya hay uno corriendo. Avisar cuando pasa eso
+# | Arrancar el proceso |
+arrancar_proceso() {
+    echo "arrancar_proceso"
+}
+
+#Invocar al script pprincipal ADVERTENCIA: no invocar el proceso si ya hay uno corriendo. Avisar cuando pasa eso
+# | Informar process id, como detener y arrancar el proceso |
+informar_resultados() {
+    MENSAJE="process id que le asigno el sistema operativo."
+    echo "$MENSAJE"
+    log_message "$MENSAJE"
+    MENSAJE="si se quiere detener el proceso se debe usar frenarproceso."
+    echo "$MENSAJE"
+    log_message "$MENSAJE"
+    MENSAJE="si luego se quiere arrancar hay que hacerlo con arrancarproceso."
+    echo "$MENSAJE"
+    log_message "$MENSAJE"
+}
+
 #Limpiamos la pantalla
 clear
 
@@ -119,16 +187,33 @@ log_message "$MENSAJE"
 echo ""
 
 #Validacion de existencia del archivo de configuración
-#ToDO
+# | Verificar configuración |
 if [ ! -f "$PATH_CONFIGURACION" ]; then
-	MENSAJE="El archivo \""$PATH_CONFIGURACION\"" no existe. Por favor vuelva a ejecutar instalarTP con la opción reparar."
+	MENSAJE="El archivo \""$PATH_CONFIGURACION\"" no existe."
+	echo "$MENSAJE"
+	log_message "$MENSAJE"
+	MENSAJE="Por favor vuelva a ejecutar instalarTP con la opción reparar."
 	echo "$MENSAJE"
 	log_message "$MENSAJE"
 else	
-	MENSAJE="El archivo \""$PATH_CONFIGURACION\"" fue encontrado. Procedemos a revisar los directorios."
+	MENSAJE="El archivo \""$PATH_CONFIGURACION\"" fue encontrado."
 	echo "$MENSAJE"
 	log_message "$MENSAJE"
+	MENSAJE="Procedemos a revisar los directorios."
+	echo "$MENSAJE"
+	log_message "$MENSAJE"
+    ##Validamos configuracion y directorios.
     validar_configuracion
+    ##Validamos archivos.
+    validar_archivos
+    ##Validamos permisos.
+    validar_permisos
+    ##Variables de ambiente
+    variables_ambiente
+    ##Arrancar el proceso
+    arrancar_proceso
+    ##Informar process id, como detener y arrancar el proceso
+    informar_resultados
 fi
 
 exit 0
