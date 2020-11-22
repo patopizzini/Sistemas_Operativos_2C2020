370962cc079fb44913079afa6ce5564e3f959926
Juan Manuel Gonzalez <juanmg0511@gmail.com>
2020-11-21 22:19:39 -0300

    frenarproceso

--- TP1/Grupo3/original/frenarproceso.sh
+++ TP1/Grupo3/original/frenarproceso.sh
@@ -9,5 +9,21 @@
 #
 #TP1 - frenaproceso
 
+PID_PPAL=$(pgrep -f pprincipal)
+if [[ "$PID_PPAL" -gt 0 ]]
+then
+	kill -15 $PID_PPAL
+	if [[ $? = 0 ]]
+	then
+		echo "Proceso principal detenido satisfactoriamente."
+	fi
+else
+	echo "El proceso principal no se encuentra corriendo."
+	echo "Fin - frenarproceso (1)"
+	exit 1
+fi
 
+#Retorno al SO
+echo ""
+echo "Fin - frenarproceso (0)"
 exit 0
