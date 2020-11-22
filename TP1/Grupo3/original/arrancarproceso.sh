e34177a338c2450a7d8437f1346c6cf0f146935c
Juan Manuel Gonzalez <juanmg0511@gmail.com>
2020-11-21 22:08:51 -0300

    arrancaproceso con todos los chequeos

--- TP1/Grupo3/original/arrancarproceso.sh
+++ TP1/Grupo3/original/arrancarproceso.sh
@@ -8,6 +8,34 @@
 #97524 - PIZZINI, PATRICIO (ppizzini@fi.uba.ar)
 #
 #TP1 - arrancaproceso
-echo "$INICIALIZAR"
-./pprincipal.sh &
+
+if [[ -f "./pprincipal.sh" ]]
+then
+	if [[ "$INICIALIZAR" == "EXITO" ]]
+	then
+		PID_PPAL=$(pgrep -f pprincipal)
+		if [[ "$PID_PPAL" -gt 0 ]]
+		then
+			echo "El proceso principal ya se encuentra corriendo, con PID: $PID_PPAL."
+			echo "Fin - arrancarproceso (1)"
+			exit
+		else
+			./pprincipal.sh &
+			PID_PPAL_LAUNCH=$!
+			echo "Proceso principal iniciado, con PID: $PID_PPAL_LAUNCH."
+		fi
+	else
+		echo "El ambiente no se encuentra inicializado. Por favor ejecute \"iniciarambiente.sh\"."
+		echo "Fin - arrancarproceso (1)"
+		exit 1
+	fi
+	else
+		"No se encuentra el archivo a ejecutar, por favor verifique su instalación."
+		echo "Fin - arrancarproceso (1)"
+		exit 1
+fi
+
+#Retorno al SO
+echo ""
+echo "Fin - arrancarproceso (0)"
 exit 0
