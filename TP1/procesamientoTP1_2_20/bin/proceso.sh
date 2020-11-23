a73fde7ca1ac1070be04a7ee9ca77823f8a3c389
sherlyporras <sherfiuba@gmail.com>
2020-11-23 06:36:45 -0300

    proc

--- TP1/procesamientoTP1_2_20/bin/proceso.sh
+++ TP1/procesamientoTP1_2_20/bin/proceso.sh
@@ -7,7 +7,7 @@ PATH_OUTPUT="../output"
 PATH_OUTPUT_COMISIONES=".../output/comisiones"
 PATH_PROCESADOS="../lotes"
 ARCH_COMERCIO="../master/comercios.csv"
-ARCH_TARJETASHOMOLOGAS="../master/comercios.csv"
+ARCH_TARJETASHOMOLOGAS="../master/tarjetashomologadas.csv"
 
 
 
@@ -28,95 +28,196 @@ done
 
 }
 function validarNombreArchivoInput(){
-    #estoy en archivo input
+#estoy en archivo input
 
 
 codComercio="$ARCH_COMERCIO"
-while IFS=',' read merchantCode col2 col3 col4
+while IFS=',' read merchantCode merchantCode_Group col3 col4
 do
+   
     if [[ "$posibleMerchantCode" == "$merchantCode" ]];#verificamos que merchantcode esta en al archivo de comercios
     then
+       grupoMerchant="$merchantCode_Group"
+       
         return 0       
     fi    
-done <"$codComercio"
+done <"$codComercio" 
 
 }
-function validarNombreTarjetasHomologadas(){
-codTarjetasHomologadas="$ARCH_TARJETASHOMOLOGAS"
-while IFS=',' read id_payment_method col2 col3 col4 col5 col6
+
+
+
+
+function validarPerteneceTarjetaHomologa(){
+
+
+codTarjeta="$ARCH_TARJETASHOMOLOGAS"
+while IFS=',' read idpay col2 col3 col4 col5 Settlement_file
 do
-    if [[ "$posibleIDpaymont" == "$id_payment_method" ]];
-    then
-         echo "esto es $id_payment_method"
-        return 0
-       
+    #echo "$idpay" #lee bien
+    
+    if [[ "$idarch" == "$idpay" ]];
+    then 
+        tipoDeTarjeta="$Settlement_file"     
+        return 0 
     fi
-done < "$codTarjetasHomologadas"
+    
+        
+
+done <"$codTarjeta" 
+
 }
+
+
 #vemos en archivo Imput/ok que son los archivos que pasaron el primerfiltro
-function verificarRegistroCabeceraYTransaccion()
+function verificarRegistroCabecera()
 {
     
     for fileAceptado in "$PATH_ACEPTADOS/"*.csv;
     do
        
-        if [ "$(ls $PATH_ACEPTADOS/)" ];
+        if [ ! "$(ls $PATH_ACEPTADOS/)" ]
         then
-            #me quedo hasta el ultimo / osea *.csv
-            #fraccionamos el nombre del archivo
-            nomarchivoAceptado="${fileAceptado##*$PATH_ACEPTADOS/}"
-            nomarchMerchantCode="${nomarchivoAceptado:1:8}"
-            #vamos por el registro de un archivo
-            registroArch="$PATH_ACEPTADOS/$nomarchivoAceptado"
-            cantidaDeRegistros=$(wc -l "$registroArch" | awk '{print $1}');
-            
-            
-            let "cantidaDeRegistros = cantidaDeRegistros - 1"
-            numregistro=1
-            while IFS=',' read col1 col2 col3 col4 posibleIDpaymont col6 col7 col8 col9 col10 col11 col12 col13 col14
-            do
-               
-                nozerocol2=$(echo $col2 | sed 's/^0*//')
-               
-                if [[ $nozerocol2 == $numregistro ]] && [[ $col1 == "TFH" ]] && [[ $col3 == "$nomarchMerchantCode" ]] && [[ $col7  == $cantidaDeRegistros ]]
-                then
-                                  
-                    cumplecabecera=0;                
-                    
-                fi
-                
-                if [[ "$cumplecabecera" == 0 ]]
-                then
-                    if validarNombreTarjetasHomologadas #validamos la columna 5 con tarjetas homologadas
-                    then
-                         if [[ $nozerocol2 == $numregistro ]] && [[ $col1 == "TFD" ]]
-                         then
-                            if [ $col12 == "000000" ] || [ $col12 == "111111" ]
-                            then
-                             return 0
-                            else
-                                mv "$fileAceptado" "$PATH_RECHAZADOS"
-                            fi
-                        fi
-                    fi 
-                fi
-                let "numregistro=numregistro+1"
-                 
-            done <"$registroArch"
+            echo "Se proceso todo aceptados"
+            return 0
         fi
+        #me quedo hasta el ultimo / osea *.csv
+        #fraccionamos el nombre del archivo
+        nomarchivoAceptado="${fileAceptado##*$PATH_ACEPTADOS/}"
+        nomarchMerchantCode="${nomarchivoAceptado:1:8}"
+        #vamos por el registro de un archivo
+        registroArch="$PATH_ACEPTADOS/$nomarchivoAceptado"
+        cantidaDeRegistros=$(wc -l "$registroArch" | awk '{print $1}');
+                  
+        let "cantidaDeRegistros = cantidaDeRegistros - 1"
+        numregistro=1
+        while IFS=',' read col1 col2 col3 col4 File_Creation_Date col6 col7 col8 col9 col10 col11 col12 col13 col14
+        do  
+            nozerocol2=$(echo $col2 | sed 's/^0*//')
+                
+            if [[ $nozerocol2 == $numregistro ]] && [[ $col1 == "TFH" ]] && [[ $col3 == "$nomarchMerchantCode" ]] && [[ $col7  == $cantidaDeRegistros ]]
+            then
+                echo "es valido cabecera"  
+                           
+                cumplecabecera=0;
+                return 0
+            else
+                echo "salio"
+                mv "$fileAceptado" "$PATH_RECHAZADOS"
+            
+            fi
+
+
+            let "numregistro=numregistro+1"
+                 
+        done <"$registroArch"
+        
+    done
+   
+}
+
+
+function verificarRegistroTransaccion()
+{
+    
+    for fileAceptado in "$PATH_ACEPTADOS/"*.csv;
+    do
+       
+        if [ ! "$(ls $PATH_ACEPTADOS/)" ]
+        then
+            echo "Se proceso todo aceptados"
+            return 0
+        fi
+        #me quedo hasta el ultimo / osea *.csv
+        #fraccionamos el nombre del archivo
+        nomarchivoAceptado="${fileAceptado##*$PATH_ACEPTADOS/}"
+        nomarchMerchantCode="${nomarchivoAceptado:1:8}"
+        #vamos por el registro de un archivo
+        registroArch="$PATH_ACEPTADOS/$nomarchivoAceptado"
+        cantidaDeRegistros=$(wc -l "$registroArch" | awk '{print $1}');
+                  
+        let "cantidaDeRegistros = cantidaDeRegistros - 1"
+        numregistro=1
+        while IFS=',' read col1 col2 posibleMerchantCode col4 idarch col6 col7 col8 col9 col10 col11 col12 col13 col14
+        do  
+            nozerocol2=$(echo $col2 | sed 's/^0*//')
+                
+            
+        if [[ $numregistro -ge 2 ]] && [[ $numregistro -lt $cantidaDeRegistros ]];
+        then
+              echo "entro"
+              echo "$idarch"
+              echo "$col1"
+            if validarPerteneceTarjetaHomologa; #validamos la columna 5 con tarjetas homologadas
+            then                          
+                if [[ $nozerocol2 == $numregistro ]] && [[ $col1 == "TFD" ]]
+                then
+                    if [ $col12 == "000000" ] || [ $col12 == "111111" ]
+                    then
+                       echo "valido"
+                       generarArchivoLiquidacionyComision
+                       
+                    else
+                       mv "$fileAceptado" "$PATH_RECHAZADOS"
+                    fi
+                fi
+            fi   
+        fi
+
+            let "numregistro=numregistro+1"
+                 
+        done <"$registroArch"
+        
+    done
+   
+}
+
+
+function generarArchivoLiquidacionyComision()
+{
+    
+    for fileAceptado in "$PATH_ACEPTADOS/"*.csv;
+    do
+       
+        if [ ! "$(ls $PATH_ACEPTADOS/)" ]
+        then
+            echo "Se proceso todo aceptados"
+            return 0
+        fi
+        #me quedo hasta el ultimo / osea *.csv
+        #fraccionamos el nombre del archivo
+        nomarchivoAceptado="${fileAceptado##*$PATH_ACEPTADOS/}"
+        nomarchMerchantCode="${nomarchivoAceptado:1:8}"
+        #vamos por el registro de un archivo
+        registroArch="$PATH_ACEPTADOS/$nomarchivoAceptado"
+
+        aaaa="${File_Creation_Date:0:4}"
+        mm="${File_Creation_Date:4:2}"
+        idTransaccion="$nomarchMerchantCode"
+        nomarchSalidaLiquidacion="$tipoDeTarjeta-$aaaa-$mm"
+        
+        nomarchSalidaComision="$grupoMerchant-$aaaa-$mm"
+        
+
+        while IFS=',' read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11 col12 col13 col14
+        do       
+
+        echo "$nomarchSalidaLiquidacion"
+        echo "$nomarchSalidaComision"
+
+                 
+        done<"$registroArch"
+        
     done
    
 }
 
 
 
-function generarArchivoLiquidacion(){
-    
 
 
 
-
-}
+#}
 
 #============Principal===================
 validarNombreLongitud
@@ -129,18 +230,23 @@ do
         posibleMerchantCode="${nomArchivo:1:8}"
         
         #validarNombreLongitud
-        if validarNombreArchivoInput
+        if validarNombreArchivoInput;
         then
-         mv "$file" "$PATH_ACEPTADOS"
-         if verificarRegistroCabeceraYTransaccion        
-         then
-            echo "paso los filtros ahora  generar informes"
-         fi
-
+        
+         mv "$file" "$PATH_ACEPTADOS"  
+             
         else
-        mv "$file" "$PATH_RECHAZADOS"
-      fi
+         mv "$file" "$PATH_RECHAZADOS"
+        fi
 
 
    fi
 done
+
+if [ "$(ls $PATH_ACEPTADOS/)" ]
+then
+    verificarRegistroCabecera; 
+    verificarRegistroTransaccion;       
+fi
+    
+
