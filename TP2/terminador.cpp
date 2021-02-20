db6f9a215b1d3ebee51256ecfb1f39b2133b408d
Juan Manuel Gonzalez <juanmg0511@gmail.com>
2021-02-20 11:29:39 -0300

    Makefile, semaforos y archivos con un *cout* solamente

---
+++ TP2/terminador.cpp
@@ -0,0 +1,21 @@
+//75.08 - 2C 2020 - GRUPO 3
+//
+//79979 - GONZALEZ, JUAN MANUEL (juagonzalez@fi.uba.ar)
+//85881 - SILVESTRI, ANDRES (asilvestri@fi.uba.ar)
+//91076 - PORRAS CARHUAMACA, SHERLY KATERIN (sporras@fi.uba.ar)
+//97524 - PIZZINI, PATRICIO (ppizzini@fi.uba.ar)
+//
+//TP2 - Ruta - Terminador
+
+#include <iostream>
+#include "Sem-sv/sv_sem.h"
+#include "Sem-sv/sv_shm.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Terminador!" << endl;
+
+	return 0;
+}
